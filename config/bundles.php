7d593ec6ae1c9927bd4d1359d2933683474b8fc1
Moacir Lima Jr <moacirjunior.eletro@gmail.com>
2020-04-12 14:22:16 -0300

    Installing orm-fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
