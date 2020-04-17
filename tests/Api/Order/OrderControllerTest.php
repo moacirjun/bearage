c3a4105814cd44aaf5e2305d2a0966f8b611db8f
Moacir Lima Jr <moacirjunior.eletro@gmail.com>
2020-04-16 22:18:39 -0300

    Listing orders test

--- tests/Api/Order/OrderControllerTest.php
+++ tests/Api/Order/OrderControllerTest.php
@@ -9,6 +9,7 @@ use App\Entity\Order\Order;
 use App\DataFixtures\OrderFixtures;
 use App\DataFixtures\ProductFixtures;
 use App\Entity\Product\Product;
+use App\Entity\Product\ProductVariant;
 
 class OrderControllerTest extends AbstractControllerTest
 {
@@ -95,5 +96,79 @@ class OrderControllerTest extends AbstractControllerTest
         $productVariant = $product->getVariants()->first();
 
         $this->assertEquals($stock - $productVariantQuantity, $productVariant->getOnHand());
+    }
+
+    public function testList()
+    {
+        $this->loadFixture([new ProductFixtures, new OrderFixtures()]);
+
+        $manager = self::$kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $order1 = $manager->getRepository(Order::class)->find(1);
+        $order2 = $manager->getRepository(Order::class)->find(2);
+        $order3 = $manager->getRepository(Order::class)->find(3);
+        $productVariant1 = $manager->getRepository(ProductVariant::class)->find(1);
+        $productVariant2 = $manager->getRepository(ProductVariant::class)->find(2);
+        $productVariant3 = $manager->getRepository(ProductVariant::class)->find(3);
+
+        $responseExpected = [
+            [
+                'id' => 1,
+                'number' => $order1->getNumber(),
+                'notes' => 'no notes 1',
+                'items' => [
+                    [
+                        'id' => $productVariant1->getCode(),
+                        'quantity' => 1,
+                        'discount' => 0,
+                        'unit' => $productVariant1->getPrice(),
+                        'tax' => 0,
+                        'total' => $productVariant1->getPrice(),
+                    ]
+                ],
+                'state' => Order::STATE_CART,
+                'order_discount' => 4,'state' => Order::STATE_CART,
+                'id' => 2,
+                'number' => $order2->getNumber(),
+                'notes' => 'no notes 2',
+                'items' => [
+                    [
+                        'id' => $productVariant2->getCode(),
+                        'quantity' => 1,
+                        'discount' => 0,
+                        'unit' => $productVariant2->getPrice(),
+                        'tax' => 0,
+                        'total' => $productVariant2->getPrice(),
+                    ]
+                ],
+                'state' => Order::STATE_CART,
+                'order_discount' => 12,
+                'order_tax' => 0,
+                'grand_total' => $productVariant2->getPrice() - 12,
+            ], [
+                'id' => 3,
+                'number' => $order3->getNumber(),
+                'notes' => 'no notes 3',
+                'items' => [
+                    [
+                        'id' => $productVariant3->getCode(),
+                        'quantity' => 1,
+                        'discount' => 0,
+                        'unit' => $productVariant3->getPrice(),
+                        'tax' => 0,
+                        'total' => $productVariant3->getPrice(),
+                    ]
+                ],
+                'state' => Order::STATE_CART,
+                'order_discount' => 0,
+                'order_tax' => 0,
+                'grand_total' => $productVariant3->getPrice(),
+            ]
+        ];
+
+        $response = $this->client->get('/api/orders');
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertResponseEquals($responseExpected, $response);
     }
 }
