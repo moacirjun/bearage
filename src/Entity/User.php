9310215be6824c06656eb377150edcf529f0f8c6
Moacir Lima Jr <moacirjunior.eletro@gmail.com>
2020-04-21 00:42:47 -0300

    Doctrine user provider configuration

---
+++ src/Entity/User.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Table(name="tb_user")
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=25, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(name="is_enabled", type="boolean")
+     */
+    private $enabled;
+
+    public function __construct(string $username, string $email)
+    {
+        $this->isActive = false;
+        $this->username = $username;
+        $this->email = $email;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function isEnabled()
+    {
+        return $this->enabled;
+    }
+
+    public function enable()
+    {
+        $this->enabled = true;
+    }
+
+    public function disable()
+    {
+        $this->enabled = false;
+    }
+
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    public function eraseCredentials(): void
+    {
+    }
+
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+}
