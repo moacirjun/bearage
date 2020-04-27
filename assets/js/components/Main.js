6bc8837bc18057f94f99cc20548f99df44fa63f5
Moacir Lima Jr <moacirjunior.eletro@gmail.com>
2020-04-26 22:27:47 -0300

    Wrap switch router inside a container

--- assets/js/components/Main.js
+++ assets/js/components/Main.js
@@ -5,16 +5,22 @@ import Orders from './Orders';
 import Products from './Products';
 import AddProducts from './AddProduct';
 import Home from './Home';
+import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
 
 const Main = () => (
     <React.Fragment>
         <Nav />
-        <Switch>
-            <Route path="/" exact><Home /></Route>
-            <Route path="/products"><Products /></Route>
-            <Route path="/orders"><Orders /></Route>
-            <Route path="/products-create"><AddProducts /></Route>
-        </Switch>
+        <Container maxWidth="sm">
+            <main>
+                <Switch>
+                    <Route path="/" exact><Home /></Route>
+                    <Route path="/products"><Products /></Route>
+                    <Route path="/orders"><Orders /></Route>
+                    <Route path="/products-create"><AddProducts /></Route>
+                </Switch>
+            </main>
+        </Container>
     </React.Fragment>
 );
 
